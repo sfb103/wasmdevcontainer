cdf2ca4ea7719b7c7c3d1be2e75861070d6b31a0
Sean Bentley <sfb103@gmail.com>
2025-01-20 00:23:21 -0800

    build improvements

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 source devcontainer.common
-echo "cont_from: $cont_from"
 echo "cont_tag: $cont_tag"
 
-docker build --load --build-arg cont_from=$cont_from -t $cont_tag -f src/Containerfile src
-
-# Using "--progress=plain" instead could be handy for debugging, since the buildkit output can be sparse
-#docker buildx build --progress=plain --load --build-arg cont_from=$cont_from -t $cont_tag -f src/Containerfile src
+docker build --load --progress=plain -t $cont_tag -f src/Containerfile src
 
 
